a7f9ddfe750be477c7e3994dbeb1fccdf92e3a98
chaochao <feixuann.li@gmail.com>
2025-09-18 20:27:02 -0700

    Init commit

---
+++ src/Container.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Textarea from "./Textarea";
+import Stats from "./Stats";
+
+export default function Container() {
+  return (
+    <main className="container">
+        <Textarea />
+        <Stats />
+    </main>
+  )
+}
---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+        <small>&copy; Copyright by Chaochao. All rights reserved. </small>
+    </footer>
+  )
+}
