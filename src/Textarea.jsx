a4966508694cc05d081ea6d7291e08d5eca2c86c
chaochao <feixuann.li@gmail.com>
2025-09-20 12:34:34 -0700

    controlled input, basic validation

--- src/Textarea.jsx
+++ src/Textarea.jsx
@@ -1,7 +1,35 @@
-import React from "react"
+import {useState} from "react";
+import Warning from './Warning';
+
 
 export default function Textarea() {
+    const [text, setText] = useState("");
+    const [warningtext, setWarningtext] = useState('');
+
+    const handleChange = (e) => {
+        let newText = e.target.value;
+        if(newText.includes('<script>')) {
+            setWarningtext('no <script> allowed')
+            newText = newText.replace('<script>', '');
+        } else if (newText.includes('@')) {
+            setWarningtext('no @ allowed');
+            newText = newText.replace('@', '');
+        } else {
+            setWarningtext('');
+        }
+
+        setText(newText);
+    }
+
   return (
-    <textarea className="textarea"/>
+    <div className="textarea">
+        <textarea onChange={handleChange} 
+            value={text}
+            placeholder="Enter your text"
+            spellCheck={false}
+        />
+        <Warning warningtext={warningtext} />
+    </div>
+   
   )
 }
---
+++ src/Warning.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Warning({ warningtext }) {
+  return (
+    <p className='warning'>{warningtext}</p>
+  )
+}
