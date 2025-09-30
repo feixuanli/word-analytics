45c216bd6ac7d3b524d434edb7a0bc015db80b1d
chaochao <feixuann.li@gmail.com>
2025-09-29 19:33:49 -0700

    stats section

--- src/Stats.jsx
+++ src/Stats.jsx
@@ -1,7 +1,21 @@
-import React from "react"
+import React from "react";
+
+const Stat = ({value, label}) => {
+    return (
+        <section className="stat">
+            <span className="stat__number">{value}</span>
+            <h2 className="second-heading">{label}</h2>
+        </section>
+    )
+}
 
 export default function Stats() {
   return (
-    <section className="stats">Stats</section>
+    <section className="stats">
+        <Stat value={0} label="words"/>
+        <Stat value={0} label="characters"/>
+        <Stat value={280} label="instagram"/>
+        <Stat value={2200} label="facebook"/>
+    </section>
   )
 }
